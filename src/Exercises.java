b4238e145029bfa4f162300650d698d755f1e862
nicolegiron1 <ngiron@ucvts.org>
2020-03-22 21:10:34 -0400

    four

--- src/Exercises.java
+++ src/Exercises.java
@@ -57,7 +57,17 @@ public class Exercises {
   }
 
   public int[] bubble(int[] list, boolean ascending) {
-    return null;
+    int n = list.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (list[j] > list[j+1]) {
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                }
+            }
+        }
+    return list;
   }
 
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
