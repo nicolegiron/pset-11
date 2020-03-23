6d27b6fb3df598d369123780f1bd5058ad49d6a7
nicolegiron1 <ngiron@ucvts.org>
2020-03-22 21:53:41 -0400

    cinco

--- src/Exercises.java
+++ src/Exercises.java
@@ -71,7 +71,17 @@ public class Exercises {
   }
 
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
-    return null;
+    int n = list.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (list.get(j).compareTo(list.get(j+1)) > 0) {
+                    String temp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, temp);
+                }
+            }
+        }
+    return list;
   }
 
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
